0f4f87810293b7bcadeef995d5ad04931ef1819e
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2026-05-24 10:26:21 -0400

    Add tablewriter library and use buffered channels

--- hb/go.mod
+++ hb/go.mod
@@ -1,3 +1,5 @@
 module hb
 
 go 1.26.3
+
+require github.com/rodaine/table v1.3.1 // indirect
--- hb/main.go
+++ hb/main.go
@@ -1,30 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/rodaine/table"
 	"net"
+	"net/http"
 	"os"
 	"sync"
 	"time"
-	"encoding/json"
 )
 
 type ToCheck struct {
-    Name string    `json:"name"`
-    Address string `json:"address"`
+	Name    string `json:"name"`
+	Address string `json:"address"`
 }
 
-func checkTCP(tc ToCheck) {
-	address := net.JoinHostPort(tc.Address, "22")
-
-	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
-	if err != nil {
-		fmt.Printf("- %s\t\t[%s]\t\tis not accessible\n", tc.Name, tc.Address)
-		return
+func checkAddr(tc ToCheck) [3]string {
+	ip := net.ParseIP(tc.Address)
+	if ip == nil {
+		resp, err := http.Get(tc.Address)
+		if err != nil {
+			return [3]string{tc.Name, tc.Address, "No"}
+		}
+		defer resp.Body.Close()
+	} else {
+		address := net.JoinHostPort(tc.Address, "22")
+		conn, err := net.DialTimeout("tcp", address, 3*time.Second)
+		if err != nil {
+			return [3]string{tc.Name, tc.Address, "No"}
+		}
+		defer conn.Close()
 	}
-	defer conn.Close()
-
-	fmt.Printf("- %s\t\t[%s]\t\tis accessible\n", tc.Name, tc.Address)
+	return [3]string{tc.Name, tc.Address, "Yes"}
 }
 
 func readConfig(path string) ([]ToCheck, error) {
@@ -59,23 +67,30 @@ func main() {
 	if len(os.Args) > 1 {
 		path = os.Args[1]
 	}
-	
+
 	tcs, err := readConfig(path)
 	if err != nil {
 		fmt.Println(fmt.Errorf("Failed to get or process config file: %w", err))
 		return
 	}
-	
+
 	const maxWorkers = 3
 
 	jobs := make(chan ToCheck)
 
+	// Buffered results channel
+	// So we don't need another goroutine to send, wait for jobs to finish and to close the channels
+	// while main goroutine is busy consuming the results channel
+	results := make(chan [3]string, len(tcs))
+
 	var wg sync.WaitGroup
 
+	// 3 worker goroutines to check addresses specified from the config!
 	for range maxWorkers {
 		wg.Go(func() {
 			for tc := range jobs {
-				checkTCP(tc)
+				result := checkAddr(tc)
+				results <- result
 			}
 		})
 	}
@@ -85,6 +100,13 @@ func main() {
 	}
 
 	close(jobs)
-
 	wg.Wait()
+	close(results)
+
+	tbl := table.New("Name", "Address", "Reachable")
+	for r := range results {
+		tbl.AddRow(r[0], r[1], r[2])
+	}
+
+	tbl.Print()
 }
