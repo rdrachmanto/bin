ccaa7152e4641294b5f3dc72d642ced57bfcb138
Rakandhiya Rachmanto <rakandhiya.daanii@gmail.com>
2026-05-24 07:13:27 -0400

    feat(hb): add reading from json structure and prettify output

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 repo_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 to_deploy=(
   "skel/build/skel"
+  "hb/bin/hb"
   "e"
 )
 
--- hb/main.go
+++ hb/main.go
@@ -3,56 +3,85 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"sync"
 	"time"
+	"encoding/json"
 )
 
-func checkTCP(ip string) {
-	address := net.JoinHostPort(ip, "22")
+type ToCheck struct {
+    Name string    `json:"name"`
+    Address string `json:"address"`
+}
+
+func checkTCP(tc ToCheck) {
+	address := net.JoinHostPort(tc.Address, "22")
 
 	conn, err := net.DialTimeout("tcp", address, 3*time.Second)
 	if err != nil {
-		fmt.Println(ip + " is not accessible")
+		fmt.Printf("- %s\t\t[%s]\t\tis not accessible\n", tc.Name, tc.Address)
 		return
 	}
 	defer conn.Close()
 
-	fmt.Println(ip + " is accessible")
+	fmt.Printf("- %s\t\t[%s]\t\tis accessible\n", tc.Name, tc.Address)
+}
+
+func readConfig(path string) ([]ToCheck, error) {
+	cfg, err := os.UserConfigDir()
+	if err != nil {
+		return nil, err
+	}
+
+	if path == "" {
+		path = cfg + "/hb/config.json"
+	}
+
+	raw, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var tcs []ToCheck
+
+	err = json.Unmarshal([]byte(raw), &tcs)
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Printf("\nReading from: %s\n\n", path)
+
+	return tcs, nil
 }
 
 func main() {
-	ips := []string{
-		"172.22.85.98",
-		"172.22.85.96",
+	path := ""
+	if len(os.Args) > 1 {
+		path = os.Args[1]
 	}
-
+	
+	tcs, err := readConfig(path)
+	if err != nil {
+		fmt.Println(fmt.Errorf("Failed to get or process config file: %w", err))
+		return
+	}
+	
 	const maxWorkers = 3
 
-	jobs := make(chan string)
+	jobs := make(chan ToCheck)
 
 	var wg sync.WaitGroup
 
 	for range maxWorkers {
-		// Old form
-		// wg.Add(1)
-		// go func() {
-		// 	defer wg.Done()
-
-		// 	for url := range jobs {
-		// 		checkUrl(url)
-		// 	}
-		// }()
-
-		// New form
 		wg.Go(func() {
-			for ip := range jobs {
-				checkTCP(ip)
+			for tc := range jobs {
+				checkTCP(tc)
 			}
 		})
 	}
 
-	for _, ip := range ips {
-		jobs <- ip
+	for _, tc := range tcs {
+		jobs <- tc
 	}
 
 	close(jobs)
